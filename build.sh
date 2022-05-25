840eef4241cef9a99e2b8b66f63efc6466b7df77
Matthew Katz <mykatz01@gmail.com>
2022-05-25 00:44:02 +0000

    Add build script

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Initial setup
+mix do deps.get, local.rebar --force, deps.compile
+
+# Remove previous assets
+mix phx.digest.clean
+
+# Compile project
+MIX_ENV=prod mix compile
+
+
+
+# Compile assets
+cd apps/block_scout_web/assets; npm install && node_modules/webpack/bin/webpack.js --mode production --progress; cd -
+cd apps/explorer && npm install; cd -
+
+mix phx.digest
+
+# Build the release and overwrite the existing release directory
+MIX_ENV=prod mix release --overwrite
