a2d6a68f13d93f5af0a8aa5cd8110ef976b7fdae
Matthew Katz <mykatz01@gmail.com>
2022-05-26 23:28:51 +0000

    Check env vars in pm2 start script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,5 +1,22 @@
-
 // PM2 ecosystem file
+
+// Verify environment variables are set
+const required_vars = [
+    "DATABASE_URL",
+    "SECRET_KEY_BASE",
+    "ETHEREUM_JSONRPC_HTTP_URL",
+    "COIN",
+    "NETWORK",
+    "SUBNETWORK",
+    "BLOCKSCOUT_HOST"
+]
+
+const unset_vars = required_vars.filter((item) => !process.env[item])
+if(unset_vars.length > 0) {
+    console.error("Required environment variables are not set")
+    console.error(unset_vars)
+    process.exit(1)
+}
 
 let apps = [
     {
@@ -8,7 +25,7 @@ let apps = [
     },
     {
         name: "Caddy reverse-proxy",
-        script: "sudo caddy reverse-proxy --from l1-explorer.constellationchain.xyz --to 127.0.0.1:4000"
+        script: `sudo caddy reverse-proxy --from ${process.env.BLOCKSCOUT_HOST} --to 127.0.0.1:4000`
     }
 ]
 
